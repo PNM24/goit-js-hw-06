9f19c147b77ec2fdeb56c6c50ff25570df7a3184
Catalin <catalin.es@hotmail.com>
2024-06-06 10:24:22 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,19 @@
+const loginData = [];
+const registerForm = document.querySelector(".login-form")
+registerForm.addEventListener("submit", handleSubmit);
 
+function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+    
+    if (email === "" || password === "") {
+      alert ("Please fill in all the fields!");
+    }
+  
+    console.log(`Login: ${email}, Password: ${password}`);
+    loginData.push({ Login: email, Password: password})
+    console.log(loginData);
+    form.reset();
+  }
