d9c2f722b5aa83ec32fbf77e4ce788228dd64b63
Catalin <catalin.es@hotmail.com>
2024-06-06 14:38:26 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+const colorDetail = document.querySelector(".color");
+const handleClick = event => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorDetail.textContent = document.body.style.backgroundColor;
+  console.log(colorDetail.textContent)
+};
+
+button.addEventListener("click", handleClick);
