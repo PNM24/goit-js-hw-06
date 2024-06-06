9ae3f6c79345d0347750f6d9251d15a0cf4423a6
Catalin <catalin.es@hotmail.com>
2024-06-06 10:20:37 +0300

    Update task-07.js

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -4,7 +4,7 @@ const textDemo = document.querySelector("#text");
 textInput.addEventListener("input", (event) => {
     let fontSizeUpdated= parseFloat(textInput.value)+'px'; 
     textDemo.style.fontSize = fontSizeUpdated;
-    // document.getElementById("text").style.fontSize = fontSizeUpdated;//a doua varianta(fara declarea de la linia 2)
+   
     console.log(textInput.value);
     console.log(textDemo.style.fontSize); 
     console.log(textDemo);
