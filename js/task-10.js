eccad462638cde2a31534f36fc6796ab6c70ecfe
Catalin <catalin.es@hotmail.com>
2024-06-06 14:40:39 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,44 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const textInput = document.querySelectorAll("input");
+
+(() => {
+    const action = {
+      createBtn: document.querySelector("[data-create]"),
+      destroyBtn: document.querySelector("[data-destroy]"),
+    };
+
+    action.createBtn.addEventListener("click", createBoxes);
+    action.destroyBtn.addEventListener("click", destroyBoxes);
+  
+
+  function createBoxes(amount) {
+    const boxes = document.querySelector("#boxes");
+    amount = document.getElementsByTagName("input")[0].value;
+    console.log(amount); //de verificare
+    const width = 20;
+    const height = 20;
+    for (let i = 1; i <= amount; i++){
+      const boxGeneration = document.createElement("div");
+      boxGeneration.style.width = width + 10*i +`px`;
+      boxGeneration.style.height = height + 10 * i + `px`;
+      boxGeneration.style.backgroundColor = getRandomHexColor();
+      boxes.append(boxGeneration);
+      console.log(boxGeneration, boxGeneration.style.width, boxGeneration.style.heigth, boxGeneration.style.backgroundColor); 
+        
+    }
+    console.log(boxes) 
+    console.log(boxes.children) 
+    }
+  
+   function destroyBoxes() {
+     boxes.innerHTML = '';
+     console.log(boxes);
+     console.log(boxes.children);
+      }
+     
+  
+ 
+})();
